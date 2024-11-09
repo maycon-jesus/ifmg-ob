04ed25210b4960c62c25056ade7ac727fc8117db
Maycon Jesus <contato@mayconjesus.dev>
2024-11-09 15:10:58 -0300

    update footballManager

--- programacao-orientada-objeto-1/footballManager/src/BeautyTable.java
+++ programacao-orientada-objeto-1/footballManager/src/BeautyTable.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 
+// Classe responsável por montar tabelas
 public class BeautyTable {
     public void printArrayList(ArrayList<ArrayList<String>> arr) {
         if (arr.isEmpty()) return;
         int[] columnsWidth = new int[arr.getFirst().size()];
 
+//        Laço responsável por mapear a largura das colunas
         for (int i = 0; i < arr.size(); i++) {
             ArrayList<String> line = arr.get(i);
             for (int j = 0; j < arr.get(i).size(); j++) {
@@ -15,9 +17,11 @@ public class BeautyTable {
             }
         }
 
+//        Laço que vai montar a tabela
         for (int i = 0; i < arr.size(); i++) {
             ArrayList<String> line = arr.get(i);
 
+//        Laço que monta a borda superior da tabela
             for (int j = 0; i == 0 && j < arr.get(i).size(); j++) {
                 int currentColumnWidth = columnsWidth[j];
 
@@ -37,6 +41,7 @@ public class BeautyTable {
                 System.out.println();
             }
 
+//            Laço que monta as linhas de dados
             for (int j = 0; j < arr.get(i).size(); j++) {
                 String cell = line.get(j);
                 int currentColumnWidth = columnsWidth[j];
@@ -47,18 +52,22 @@ public class BeautyTable {
                 System.out.print(cell + " ".repeat(whiteSpaces) + " │ ");
             }
             System.out.println();
+
+//            Laço que monta a borda inferior de cada celula ou da tabela
             for (int j = 0; j < arr.get(i).size(); j++) {
                 int currentColumnWidth = columnsWidth[j];
 
                 if (i == arr.size() - 1 && j == 0) {
+//                    Borda inferior da tabela
                     System.out.print("└");
                 } else if (j == 0) {
+//                    Borda inferior de celula
                     System.out.print("├");
                 }
 
                 System.out.print("─".repeat(currentColumnWidth + 2));
 
-                // Faz ultima linha da tabela
+//                    Borda inferior da tabela
                 if (i == arr.size() - 1 && j == line.size() - 1) {
                     System.out.print("┘");
                     continue;
@@ -67,7 +76,7 @@ public class BeautyTable {
                     continue;
                 }
 
-                // Faz linhas intermediarias
+                // Borda inferior das celulas
                 if (j == line.size() - 1) {
                     System.out.print("┤");
                 } else {
