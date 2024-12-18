09d7f02e81a6f206842a40b9c952d00ea0f57cbf
Maycon Jesus <contato@mayconjesus.dev>
2024-12-18 15:13:42 -0300

    feat: selection sort

--- analise-estrutura-dados-1/lista-aeds2/src/Main.java
+++ analise-estrutura-dados-1/lista-aeds2/src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 	public static void main(String[] args) {
 		SortResponse resp = InsertionSort.sort(new int[]{3, 2,1});
 		System.out.println(resp.interactions);
+
+		SortResponse resp2 = SelectionSort.sort(new int[]{3, 2,1});
+		System.out.println(resp2.interactions);
 	}
 }
