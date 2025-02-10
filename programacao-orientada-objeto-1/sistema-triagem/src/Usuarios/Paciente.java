f7bb1a4acba4ad9ac6a3922d05575908c002de5d
Vitor Sanson <vhsanson@gmail.com>
2025-02-10 19:25:10 -0300

    Classe Paciente

--- programacao-orientada-objeto-1/sistema-triagem/src/Usuarios/Paciente.java
+++ programacao-orientada-objeto-1/sistema-triagem/src/Usuarios/Paciente.java
@@ -1,4 +1,60 @@
 package Usuarios;
 
-public class Paciente {
+
+import java.util.Date;
+
+public class Paciente extends Pessoa {
+
+   Date dataNascimento;
+   String sexo;
+   String nomeMae;
+    String endereco;
+    String email;
+    String nacinalidade
+    String telefone;
+    String cartaoDoSus;
+
+    public Paciente(int id, String nomeCompleto, String cpf, Date dataNascimento, String sexo, String nomeMae, String endereco, String email, String nacinalidade, String telefone, String cartaoDoSus) {
+        super(id, nomeCompleto, cpf, UserType.PACIENTE);
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+        this.nomeMae = nomeMae;
+        this.endereco = endereco;
+        this.email = email;
+        this.nacinalidade = nacinalidade;
+        this.telefone = telefone;
+        this.cartaoDoSus = cartaoDoSus;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public String getNomeMae() {
+        return nomeMae;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNacinalidade() {
+        return nacinalidade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getCartaoDoSus() {
+        return cartaoDoSus;
+    }
 }
