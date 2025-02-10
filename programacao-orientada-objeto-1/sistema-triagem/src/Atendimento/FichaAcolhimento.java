01b21a825a1f87233d27c5cb0527ef1e49b08337
Vitor Sanson <vhsanson@gmail.com>
2025-02-10 19:58:03 -0300

    Classe Paciente

---
+++ programacao-orientada-objeto-1/sistema-triagem/src/Atendimento/FichaAcolhimento.java
@@ -0,0 +1,35 @@
+package Atendimento;
+
+import Usuarios.MedicoEspecialidade;
+
+public class FichaAcolhimento {
+    //int id;
+    int enfermeiroId;
+    String Sintomas;
+    String tempoDosSintomas;
+    String doencasPreExistentes;
+    String usoDeMedicamento;
+    String jaFezCirurgia;
+    String AlergiaMedicamentoSubstancia;
+    String gravida;
+    String traumaAcidenteRecente;
+    String historicoFamiliar;
+
+    Boolean fumante;
+    Boolean consomeAlcool;
+
+    String infoDor;
+    double temperaturaCorporal;
+    Sring pressaoSanguinea;
+    int frequeciaCardiaca;
+    double frequenciaRespiratoria;
+    int saturacaoDeOxigenio;
+    int taxaDeGlicose;
+    boolean consegueAndar;
+    String sinaisDeEmergencia;
+    String sinaisDeInfeccaoGrave;
+    MedicoEspecialidade especialidadeNecessaria;
+    PrioridadeManchester prioridade;
+
+
+}
